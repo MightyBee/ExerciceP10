8d79143ea1f93827398715ca21b9a6c975b26f6c
Simon Merkt <simon.merkt@epfl.ch>
2018-05-20 19:34:49 +0200

    b

--- Qt_GL/main_qt_gl.cc
+++ Qt_GL/main_qt_gl.cc
@@ -12,19 +12,19 @@ int main(int argc, char* argv[]){
     IntegrateurNewmark newmark;
     IntegrateurRungeKutta rk;
 
-    //Pendule p1({0},{1},{-1,0,0},{0,1,0},2,2);
+    //Pendule p1({0},{1},{-1,0,0},{0,1,0},1,1);
     //Pendule p2({M_PI-0.00001},{0},{1,1,0},{1,0,0},1,1,0.05);
     //Ressort r({1}, {0}, {-1,2,-1}, {0,0,0}, 2, 1);
-    //Chariot c({0,0},{4,0},{1,-2,0},{-1,-1,0},100,10,0,1,1,0);
+    Chariot c({0,0},{4,0},{1,-2,0},{-1,-1,0},100,10,0,1,1,0);
     GLWidget w(rk);
     //w.initializeSysteme();
     //w.add(PenduleDouble({3.14,3.14}));
-    //w.add(PenduleDouble({1.5,1.5},{0,0},{1,0,0},{0,1,0},1,1,10,2));
-    w.add(PenduleRessort({2,1},{0,0},{2,1,0},{0,0,0},0.5,1,10));
+    //w.add(PenduleDouble({1.5,1.5},{0,0},{1,0,0},{0,1,0},1,1,1,1));
+    //w.add(PenduleRessort({2,1},{0,0},{2,1,0},{0,0,0},0.5,1,10));
     //w.add(p1);
     //w.add(p2);
     //w.add(r);
-    //w.add(c);
+    w.add(c);
     w.show();
     return a.exec();
   } catch(Erreur err){
